3535e262e4a357935cffdc3db481d836b5b2d233
Naomi <naomi.m.palm@gmail.com>
2021-06-06 01:25:34 -0400

    Initial commit

---
+++ src/main/java/ex03base/app.java
@@ -0,0 +1,29 @@
+package ex03base;
+
+import java.util.Scanner;
+
+public class app {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        app newApp = new app();
+
+        System.out.println("What is the quote?: ");
+        String quote = input.nextLine();
+        System.out.println("Who said it?: ");
+        String whoSaid = input.nextLine();
+
+
+        String outPut = newApp.stringConCat(quote, whoSaid);
+
+        System.out.println(outPut);
+
+
+    }
+
+    private String stringConCat(String tempQuote, String who){
+        return "" + who + " says, \"" + tempQuote + "\"";
+    }
+}
